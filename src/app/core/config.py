767229c8706009bccba35f2a2a1b7589e011ecf4
suhyeon <102345466+jeong-su-hyeon@users.noreply.github.com>
2025-05-22 22:28:48 +0900

    Add 발음 교정 기능
    1) MS Azure STT 연결
    2) 발음 교정 API 구현

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -5,6 +5,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     openai_api_key: str
     dalle_api_key: str
+    stt_api_key: str
 
     class Config:
         env_file = ".env"   # 기본 경로 설정 (.env에서 불러옴)
